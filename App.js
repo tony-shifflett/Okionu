997725c0f3d8a26c6f0be36d9c54c20c7918ac1a
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-25 17:49:25 -0500

    added icon to BackHeaderGreen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { View } from 'react-native';
 const App = () => {
   return (
     <View>
-      
+
     </View>
   )
 }
--- components/headers/BackHeaderGreen.js
+++ components/headers/BackHeaderGreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/dist/FontAwesome'
 
 const BackHeaderGreen = ({title}) => {
   return (
     <View style={styles.header}>
         <Text style={styles.text}>{title}</Text>
+        <Icon name="arrow-left"/>
     </View>
   )
 }
