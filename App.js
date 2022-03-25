4f323210b2eb62076ed5ba9e9ad02f01cca6367b
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-25 15:47:03 -0500

    created SearchBox

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
-// import GreenHeader from './components/headers/GreenHeader'
-// import LogoHeader from './components/headers/LogoHeader';
-// import StandardButton from './components/StandardButton';
-import BackHeaderGreen from './components/headers/BackHeaderGreen';
 
 const App = () => {
   return (
     <View>
-      <BackHeaderGreen/>
     </View>
   )
 }
